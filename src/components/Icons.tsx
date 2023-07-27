a5330598f3191c5e131c9936b5255180306ec60b
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-07-27 08:12:45 +0800

    58

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,4 +1,5 @@
 import { LucideProps } from "lucide-react";
+import { FcGoogle } from "react-icons/fc";
 
 export const Icons = {
   logo: (props: LucideProps) => (
@@ -30,4 +31,27 @@ export const Icons = {
       </g>
     </svg>
   ),
+  logo2: (props: LucideProps) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="m10 10-2 2 2 2" />
+      <path d="m14 14 2-2-2-2" />
+      <path d="M5 21a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2" />
+      <path d="M9 21h1" />
+      <path d="M14 21h1" />
+    </svg>
+  ),
+  google: (props: React.HTMLAttributes<SVGElement>) => {
+    return <FcGoogle {...props} />;
+  },
 };
